28c7e3a8fed2c520ca1c309b58220bea41766c3b
Audrey Tang <audreyt@audreyt.org>
2014-07-20 22:29:03 +0800

    * Sync upstream

--- platforms/android/gen/R.java.d
+++ platforms/android/gen/R.java.d
@@ -46,7 +46,6 @@
 /Users/audreyt/w/moedict-webkit/app/platforms/android/res/layout/main.xml \
 /Users/audreyt/w/moedict-webkit/app/platforms/android/res/menu/example.xml \
 /Users/audreyt/w/moedict-webkit/app/platforms/android/res/values/strings.xml \
-/Users/audreyt/w/moedict-webkit/app/platforms/android/res/xml/config.xml \
 /Users/audreyt/w/moedict-webkit/app/platforms/android/bin/res/drawable/down.png \
 /Users/audreyt/w/moedict-webkit/app/platforms/android/bin/res/drawable/icon.png \
 /Users/audreyt/w/moedict-webkit/app/platforms/android/bin/res/drawable/info.png \
