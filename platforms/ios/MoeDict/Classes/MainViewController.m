f41334b540c1ab2b709d656f7ec333f2b118626d
Lin Zhemin <lin.zhemin@gmail.com>
2015-08-22 18:35:28 +0800

    merge again QQ

--- platforms/ios/MoeDict/Classes/MainViewController.m
+++ platforms/ios/MoeDict/Classes/MainViewController.m
@@ -19,11 +19,7 @@
 
 //
 //  MainViewController.h
-<<<<<<< HEAD
-//  HelloCordova
-=======
 //  __PROJECT_NAME__
->>>>>>> master
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
