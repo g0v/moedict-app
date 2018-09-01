53f24fdb15a13925dcc427ad64d82103b6bec9e0
Audrey Tang <audreyt@audreyt.org>
2018-09-01 10:05:25 +0800

    * Add splash screen support

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 android-release ::
 	cordova build android
+	jarsigner -verbose -tsa http://timestamp.digicert.com -tsacert Socialtext -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/Socialtext.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk Socialtext
+	zipalign -fv 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk app-release.apk
--- platforms/android/app/build/generated/source/buildConfig/debug/org/audreyt/dict/moe/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/org/audreyt/dict/moe/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.audreyt.dict.moe";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10000;
-  public static final String VERSION_NAME = "1.0.0";
+  public static final int VERSION_CODE = 50600;
+  public static final String VERSION_NAME = "5.6.0";
 }
