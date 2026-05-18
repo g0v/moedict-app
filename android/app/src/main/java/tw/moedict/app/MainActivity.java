a0c41ec871997962de9fec4a34dc1600717f92ca
Bestian Tang <109048+bestian@users.noreply.github.com>
2026-05-18 15:03:01 +0800

    鎖定 WebView 字級縮放，防止 Android 系統字級設定壓板

--- android/app/src/main/java/tw/moedict/app/MainActivity.java
+++ android/app/src/main/java/tw/moedict/app/MainActivity.java
@@ -1,5 +1,15 @@
 package tw.moedict.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 鎖定 WebView 字級縮放為 100%，防止 Android 系統字級設定壓板導航列 (fixes #114)
+        WebSettings settings = this.bridge.getWebView().getSettings();
+        settings.setTextZoom(100);
+    }
+}
